78f7a4b9fc9bb342fd14014e6e80a921cccd3856
JerrySippy <104254392+JerrySippy@users.noreply.github.com>
2023-05-19 13:20:24 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 //https://teachablemachine.withgoogle.com/models/_3BGR-cjp/
+
+function startClassification() {
+  navigator.mediaDevices.getUserMedia({ audio: true });
+  classifier = ml5.soundClassifier(
+    "https://teachablemachine.withgoogle.com/models/_3BGR-cjp/model.json",
+    modelReady
+  );
+}
+function modelReady(){
+
+    classifier.classify(gotResults);
+}
